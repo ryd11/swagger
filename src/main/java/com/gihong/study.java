0b719c43ee7d9aaf5a3dc915dbd1d3d21cbe6a0c
ryd11 <sdmee97@gmail.com>
2024-05-13 21:11:27 +0900

    first setting
    d

---
+++ src/main/java/com/gihong/study.java
@@ -0,0 +1,12 @@
+package com.gihong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class study {
+
+    public static void main(String[] args) {
+        SpringApplication.run(study.class, args);
+    }
+}
